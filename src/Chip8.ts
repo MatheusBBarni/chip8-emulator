ef76aa572f9bf239aa5043b57ad31a953828ce6b
MatheusBBarni <brehm.matheus@hotmail.com>
2022-06-26 00:24:09 -0300

    feat: created Memory class

--- src/Chip8.ts
+++ src/Chip8.ts
@@ -1,9 +1,12 @@
 import { Display } from "./Display";
+import { Memory } from "./Memory";
 
 export class Chip8 {
+  private memory: Memory;
   private display: Display;
 
   constructor() {
+    this.memory = new Memory();
     this.display = new Display();
   }
 }
---
+++ src/Memory.ts
@@ -0,0 +1,32 @@
+import { MEMORY_SIZE } from "./constants/memoryConstants";
+
+export class Memory {
+  private memory: Uint8Array;
+
+  constructor() {
+    this.memory = new Uint8Array(MEMORY_SIZE);
+
+    this.reset();
+  }
+
+  private reset(): void {
+    this.memory.fill(0);
+  }
+
+  setMemory(index: number, value: number): void {
+    this.assertMemory(index);
+    this.memory[index] = value;
+  }
+
+  getMemory(index: number): number {
+    this.assertMemory(index);
+    return this.memory[index];
+  }
+
+  assertMemory(index: number) {
+    console.assert(
+      index >= 0 && index <= MEMORY_SIZE,
+      `Error: trying to access memory at index: ${index}`
+    );
+  }
+}
